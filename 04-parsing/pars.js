9efd4e5ebbb6407d529931725ec3f10abe6eb065
darkFoxyy <dragoha2000@gmail.com>
2020-02-12 20:55:35 +0300

    Even more fixes

--- 04-parsing/pars.js
+++ 04-parsing/pars.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 
+
 const data = fs.readFile('contacts.csv', (err, data) => { // If it's running from folder 04-parsing, else there should be it's full path 
     const content = data.toString()
     const rows = content.split('\r\n')
@@ -25,26 +26,30 @@ const data = fs.readFile('contacts.csv', (err, data) => { // If it's running fro
         const content = data.toString()
         const rows = content.split('\r\n')
 
-        const contactsForJson = []
         const keys = new Set()
         rows.forEach(row => {
             const data = row.split(',')
             const object = contacts.get(data[2])
-
-            if (object && !keys.has(data[2])) {
+            
+            if (object) {
                 const phone = {}
 
                 phone[data[9]] = data[8]
                 object.phones.push(phone)
 
-                const key = {}
-                key[data[2]] = object
-
                 keys.add(data[2])
 
-                contactsForJson.push(key)
+                contacts.set(data[2],object)
             }
         });
+        const contactsForJson = []
+
+        keys.forEach(key => {
+            const object = {}
+            object[key] = contacts.get(key)
+            contactsForJson.push(object)
+        })
+
         const jsonObject = JSON.stringify(contactsForJson, null, 2)
         fs.writeFileSync('data.json', jsonObject)
     })
